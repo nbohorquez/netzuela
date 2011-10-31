06e9b27cb0bde626e9529510c395d910734cbc5b
Nestor Bohorquez <tca7410nb@gmail.com>
2011-10-31 18:00:32 +0000

    git-svn-id: http://jarcob:8080/svn/Spuria/LineaBase@3 a983820b-8ef5-854c-b72c-8da5e8f5abee

--- 3210_Spuria.sql
+++ 3210_Spuria.sql
@@ -2,16 +2,16 @@ DROP DATABASE IF EXISTS `Spuria`;
 
 /* Debe ejecutarse en este orden */
 
-SOURCE D:\Netzuela\3210_Spuria\3210_Esqueleto.sql
+SOURCE D:\Netzuela\Spuria\Esqueleto.sql
 
-SOURCE D:\Netzuela\3210_Spuria\3210_Funciones padres.sql
-SOURCE D:\Netzuela\3210_Spuria\3210_Funciones sobre productos.sql
-SOURCE D:\Netzuela\3210_Spuria\3210_Funciones sobre publicidades.sql
-SOURCE D:\Netzuela\3210_Spuria\3210_Funciones sobre regiones geograficas.sql
-SOURCE D:\Netzuela\3210_Spuria\3210_Funciones sobre usuarios.sql
-SOURCE D:\Netzuela\3210_Spuria\3210_Funciones sobre estadisticas.sql
-SOURCE D:\Netzuela\3210_Spuria\3210_Funciones sobre ventas.sql
-SOURCE D:\Netzuela\3210_Spuria\3210_Funciones miscelaneas.sql
-SOURCE D:\Netzuela\3210_Spuria\3210_Funciones sobre registro.sql
+SOURCE D:\Netzuela\Spuria\Funciones padres.sql
+SOURCE D:\Netzuela\Spuria\Funciones sobre productos.sql
+SOURCE D:\Netzuela\Spuria\Funciones sobre publicidades.sql
+SOURCE D:\Netzuela\Spuria\Funciones sobre regiones geograficas.sql
+SOURCE D:\Netzuela\Spuria\Funciones sobre usuarios.sql
+SOURCE D:\Netzuela\Spuria\Funciones sobre estadisticas.sql
+SOURCE D:\Netzuela\Spuria\Funciones sobre ventas.sql
+SOURCE D:\Netzuela\Spuria\Funciones miscelaneas.sql
+SOURCE D:\Netzuela\Spuria\Funciones sobre registro.sql
 
-SOURCE D:\Netzuela\3210_Spuria\3210_Inicializacion.sql
+SOURCE D:\Netzuela\Spuria\Inicializacion.sql
