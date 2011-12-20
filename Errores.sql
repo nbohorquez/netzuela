ede814895f624dd841cb1999a7cbbc9943428b23
Nestor Bohorquez <tca7410nb@gmail.com>
2011-12-20 19:46:36 +0000

    Movi la ubicacion del repositorio de D:/ a D:/Dropbox/ empleando "Relocate"
    git-svn-id: http://jarcob:8080/svn/Spuria/LineaBase@9 a983820b-8ef5-854c-b72c-8da5e8f5abee

--- Errores.sql
+++ Errores.sql
@@ -4,6 +4,6 @@ DECLARE Error_de_clave_externa CONDITION FOR 1452;
 DECLARE Error_de_clave_duplicada CONDITION FOR 1052;
 DECLARE Error_de_registro_no_encontrado CONDITION FOR 1329;
 
-/***********************************************************/
+/**********************************************************/
 DELIMITER ;
-/***********************************************************/
+/**********************************************************/
