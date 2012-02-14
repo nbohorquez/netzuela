d9911edbf1ae23794bbc995a4586100e8524b327
Nestor Bohorquez <tca7410nb@gmail.com>
2012-02-14 16:58:20 -0430

    Cree un nuevo proyecto cuya funcion es administrar el objeto a traves del cual el resto de los componentes de Spuria acceden a la base de datos.

---
+++ Datos/Proveedor.cs
@@ -0,0 +1,34 @@
+﻿namespace Zuliaworks.Netzuela.Spuria.Datos
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    
+    public class Proveedor
+    {
+        #region Variables
+
+        private static SpuriaEntities spuria;
+
+        #endregion
+
+        #region Constructores
+
+        static Proveedor()
+        {
+            spuria = new SpuriaEntities();
+        }
+
+        #endregion
+
+        #region Propiedades
+
+        public static SpuriaEntities Spuria
+        {
+            get { return spuria; }
+        }
+
+        #endregion
+    }
+}
